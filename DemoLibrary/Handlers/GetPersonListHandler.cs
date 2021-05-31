8b3f852f3edb6940b2b6845de98f03084780c3e6
Parsa <mehdipourparsa@gmail.com>
2021-05-31 14:39:33 +0430

    feat : add get person list query

---
+++ DemoLibrary/Handlers/GetPersonListHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using DemoLibrary.DataAccessSimulation;
+using DemoLibrary.Models;
+using DemoLibrary.Queries;
+using MediatR;
+
+namespace DemoLibrary.Handlers
+{
+    public class GetPersonListHandler : IRequestHandler<GetPersonListQuery, List<Person>>
+    {
+        private readonly IDataAccess _dataAccess;
+
+        public GetPersonListHandler(IDataAccess dataAccess)
+        {
+            _dataAccess = dataAccess;
+        }
+
+        public Task<List<Person>> Handle(GetPersonListQuery request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_dataAccess.Get());
+        }
+    }
+}
