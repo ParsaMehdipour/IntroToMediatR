5fa097d49119241ac8cb5db8121404646fd7e9d4
Parsa <mehdipourparsa@gmail.com>
2021-05-31 14:40:03 +0430

    feat : add get person list query

---
+++ DemoLibrary/Queries/GetPersonListQuery.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using DemoLibrary.Models;
+using MediatR;
+
+namespace DemoLibrary.Queries
+{
+    public class GetPersonListQuery :IRequest<List<Person>>
+    {
+
+    }
+}
